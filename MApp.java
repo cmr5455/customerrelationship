46861401bb059d71a1b7564a59d8876373a0b741
Cozda <74552411+Cozda@users.noreply.github.com>
2020-11-16 13:25:43 -0500

    Add files via upload

---
+++ MApp.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MApp extends JFrame implements ActionListener{
+
+	public static void main(String[] args) {
+		new MApp();
+	}
+
+}
---
+++ VMapp.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class VMapp extends JFrame implements ActionListener{
+
+	public static void main(String[] args) {
+		new VMapp();
+
+	}
+
+}
